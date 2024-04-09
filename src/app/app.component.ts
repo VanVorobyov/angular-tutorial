a522d14f2171aafb9e27e25b533fc4b10daa9c12
Ivan Vorobyov <ivanvorobyov1992@yandex.ru>
2024-04-09 23:03:27 +0300

    learn how add second component and use it in first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,23 @@
 import { Component } from '@angular/core'
+import { HomeComponent } from './home/home.component'
 import { RouterOutlet } from '@angular/router'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [HomeComponent, RouterOutlet],
   template: `
-    <h1>Default</h1>
+    <main>
+      <header class='brand-name'>
+        <img class='brand-logo' src='/favicon.ico' alt='logo' aria-hidden='true' />
+      </header>
+      <section class='content'>
+        <app-home></app-home>
+      </section>
+    </main>
   `,
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'angular-tutorial'
+  title = 'homes'
 }
